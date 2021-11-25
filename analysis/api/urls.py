779636b5804a210576ed59e81fd4799d66db0e5b
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-25 23:52:40 +0330

    feat (analysis--urls): Add urls

--- analysis/api/urls.py
+++ analysis/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from analysis.api.views.report_view import ReportViewset
+from analysis.api.views.report_view import ReportViewset, TotalReportViewset
 from analysis.api.views.saqr_view import SaqrViewset
 
 router = DefaultRouter()
 router.register(r'saqrs', SaqrViewset, basename='saqrs')
 router.register(r'reports', ReportViewset, basename='reports')
+router.register(r'total-reports', TotalReportViewset, basename='total-reports')
 
 urlpatterns = router.urls
