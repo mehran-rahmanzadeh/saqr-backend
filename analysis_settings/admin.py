e5493e8304df95cdf257b875eb5d35803515731b
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-26 01:44:40 +0330

    feat (analysis_settings--admin): Create admin

--- analysis_settings/admin.py
+++ analysis_settings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from analysis_settings.models import Parameters
+from analysis_settings.models import Parameters, AnalyseReview
 
 
 @admin.register(Parameters)
@@ -18,3 +18,26 @@ class ParametersModelAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return request.user.is_superuser and not self.model.objects.exists()
+
+
+@admin.register(AnalyseReview)
+class AnalyseReviewModelAdmin(admin.ModelAdmin):
+    list_display = ['submitted_by', 'reason', 'created']
+
+    filter_horizontal = ['reports']
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        obj.submitted_by = request.user
+        super().save_model(request, obj, form, change)
