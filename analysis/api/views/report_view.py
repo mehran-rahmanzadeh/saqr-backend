5e5a32bdd587e3119f58fedada3dc6d2550034f5
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-25 23:52:10 +0330

    feat (analysis--views): Add public reports view
    - order reports by parameters
    - limit response

--- analysis/api/views/report_view.py
+++ analysis/api/views/report_view.py
@@ -1,8 +1,13 @@
-from rest_framework.mixins import RetrieveModelMixin
-from rest_framework.permissions import IsAuthenticated
+from django.conf import settings
+from rest_framework.filters import OrderingFilter
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import GenericViewSet
 
-from analysis.api.serializers.report_serializer import ReportSerializer
+from analysis.api.serializers.report_serializer import (
+    ReportSerializer,
+    MinimizedReportSerializer
+)
 from analysis.models.report_model import Report
 
 
@@ -10,5 +15,29 @@ class ReportViewset(GenericViewSet, RetrieveModelMixin):
     """Report Viewset"""
     serializer_class = ReportSerializer
     permission_classes = (IsAuthenticated,)
-    queryset = Report.objects.all()
+    queryset = Report.objects.select_related('report_detail')
     lookup_field = 'sku'
+
+
+class TotalReportViewset(GenericViewSet, ListModelMixin):
+    """Total Report Viewset"""
+    serializer_class = MinimizedReportSerializer
+    permission_classes = (AllowAny,)
+    queryset = Report.objects.select_related('saqr', 'report_detail')
+    filter_backends = [OrderingFilter]
+    ordering_fields = (
+        'report_detail__max_speed',
+        'report_detail__avg_speed',
+        'report_detail__max_accel',
+        'report_detail__avg_accel',
+        'report_detail__max_alt',
+        'report_detail__avg_alt',
+        'report_detail__score'
+    )
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        """limit number of instances showing in public response"""
+        limit_response_count = settings.LIMIT_PUBLIC_REPORT_COUNT
+        response.data['count'] = limit_response_count if response.data['count'] > limit_response_count else response.data['count']
+        response.data['results'] = response.data['results'][:limit_response_count]
+        return super().finalize_response(request, response, *args, **kwargs)
