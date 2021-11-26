78a83f7940edb47bcb19447227e7067e792bf8a3
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-26 14:21:52 +0330

    feat (users--admin): Improve user admin

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
+from analysis.admin import SaqrOwnerTabularInline, ReportSubmittedTabularInline
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = ('username', 'email', 'is_verified')
-    list_filter = ('is_verified', 'is_staff', 'is_active', 'last_login', 'date_joined')
+    list_filter = (
+        'is_verified', 'is_operator', 'is_staff',
+        'is_active', 'last_login', 'date_joined'
+    )
     search_fields = ('username', 'email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 'is_verified', 'is_operator',
+                'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    filter_horizontal = ('groups', 'user_permissions',)
+
+    inlines = [
+        SaqrOwnerTabularInline,
+        ReportSubmittedTabularInline
+    ]
