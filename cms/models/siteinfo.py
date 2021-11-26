b6b41f67bd50dd3e31840a4dbe48c3905247cfbc
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-26 17:33:05 +0330

    feat (cms--models): Add properties

--- cms/models/siteinfo.py
+++ cms/models/siteinfo.py
@@ -7,8 +7,8 @@ from django.utils.translation import ugettext_lazy as _
 
 # validator support
 from importlib import import_module
-validators = import_module('django.core.validators')
 
+validators = import_module('django.core.validators')
 
 # cache support
 from cms.mixins import ModelCacheMixin
@@ -19,90 +19,106 @@ class SiteInfo(models.Model, ModelCacheMixin):
     SiteInfo Model
     Auto generated
     """
-    
+
     CACHE_KEY = 'siteinfo'  # auto generated CACHE_KEY
-    
+
     logo = models.ImageField(
-             
-             validators=[
-                
-                getattr(
-                    validators,
-                    'FileExtensionValidator')(
-                    allowed_extensions=[
-                        'svg',
-                        'SVG',
-                        'webp',
-                        'png',
-                        'PNG']),
-                
-             ]
-             
+
+        validators=[
+
+            getattr(
+                validators,
+                'FileExtensionValidator')(
+                allowed_extensions=[
+                    'svg',
+                    'SVG',
+                    'webp',
+                    'png',
+                    'PNG']),
+
+        ]
+
     )
-    
+
     tel = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     email = models.CharField(
-             max_length=255,
-             
-             validators=[
-                
-                getattr(validators, 'EmailValidator')(None),
-                
-             ]
-             
+        max_length=255,
+
+        validators=[
+
+            getattr(validators, 'EmailValidator')(None),
+
+        ]
+
     )
-    
+
     address = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     instagram = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     facebook = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     twitter = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     linkedin = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     whatsapp = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     footer = models.CharField(
-             max_length=255,
-             
+        max_length=255,
+
     )
-    
+
     created = models.DateTimeField(
-             auto_now_add=True,
-             
+        auto_now_add=True,
+
     )
-    
+
     modified = models.DateTimeField(
-             auto_now=True,
-             
+        auto_now=True,
+
     )
-    
+
     def __str__(self):
-        return f"SiteInfo {self.pk}" 
+        return f"SiteInfo {self.pk}"
+
+    @property
+    def get_instagram_url(self):
+        return f'https://www.instagram.com/{self.instagram}/'
+
+    @property
+    def get_whatsapp_url(self):
+        return f'https://www.whatsapp.com/{self.whatsapp}/'
+
+    @property
+    def get_twitter_url(self):
+        return f'https://www.twitter.com/{self.twitter}/'
+
+    @property
+    def get_facebook_url(self):
+        return f'https://www.facebook.com/{self.facebook}/'
 
     class Meta:
         verbose_name = _("Site info")  # auto generated verbose_name
