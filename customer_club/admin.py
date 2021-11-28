014289e9c2f6e476bae1e7af419e0869fd1d239b
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-28 17:37:21 +0330

    feat (customer_club--admin): Add admin

---
+++ customer_club/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from customer_club.models import Category, Question
+
+
+@admin.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'created', 'modified']
+    list_filter = ['created', 'modified']
+    search_fields = ['title', 'description']
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title_en', 'title_ar',
+                'description_en', 'description_ar',
+                'image', 'parent'
+            )
+        }),
+    )
+
+
+@admin.register(Question)
+class QuestionModelAdmin(admin.ModelAdmin):
+    list_display = ['question', 'created', 'modified']
+    list_filter = ['created', 'modified']
+    search_fields = ['question', 'answer']
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'question_en', 'question_ar',
+                'answer_en', 'answer_ar'
+            )
+        }),
+    )
