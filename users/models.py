e96d7244909d143b9703b6947537ba56e2f5017b
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-28 16:56:13 +0330

    feat (users--models): Add fields
    - phone_number
    - address
    - birth_date

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+from painless.utils.regex.patterns import INTERNATIONAL_PHONE_NUMBER_PATTERN
 
 
 class User(AbstractUser):
@@ -21,6 +24,25 @@ class User(AbstractUser):
     is_operator = models.BooleanField(
         _('Is Operator'),
         default=False
+    )
+    phone_number = models.CharField(
+        _('Phone Number'),
+        null=True,
+        blank=True,
+        max_length=15,
+        validators=[
+            RegexValidator(INTERNATIONAL_PHONE_NUMBER_PATTERN)
+        ]
+    )
+    address = models.TextField(
+        _('Address'),
+        null=True,
+        blank=True
+    )
+    birth_date = models.DateField(
+        _('Birth Date'),
+        null=True,
+        blank=True
     )
 
     class Meta:
