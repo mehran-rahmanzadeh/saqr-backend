d49529d83725933eae27246301a8d289c9d97fc6
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-12-01 14:47:15 +0330

    fix (auth--tasks): Fix send mail task

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -1,10 +1,11 @@
 from celery import shared_task
 
-from django.core.mail import EmailMessage
+from django.conf import settings
+from django.core.mail import send_mail as django_send_mail
 
 
 @shared_task
 def send_mail(email, subject, message):
     """send email"""
-    mail = EmailMessage(subject, message, to=[email])
-    mail.send()
+    if email:
+        django_send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
